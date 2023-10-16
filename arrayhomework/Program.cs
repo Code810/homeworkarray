861cb2d735a1d8375c367b037667c033c522f3af
code810 <nadirssh@code.edu.az>
2023-10-16 12:51:40 +0400

    task1newmetod

--- arrayhomework/Program.cs
+++ arrayhomework/Program.cs
@@ -1,25 +1,46 @@
-﻿#region homework task1
+﻿
+#region homework task1
 
-using System.ComponentModel.Design;
+//Console.WriteLine("reqemleri daxil edin:");
+//int num1 = int.Parse(Console.ReadLine());
+//int num2 = int.Parse(Console.ReadLine());
+//int sum=num1+ num2;
 
-int num1 = int.Parse(Console.ReadLine());
+//    if (sum <= 99 && sum >=10)
+//    {
+//        Console.WriteLine("Cem 2 reqemli ededdir");
+//    }
+
+//    else if (sum>=100 && sum <= 999)
+//    {
+//        Console.WriteLine("Cem 3 reqemli ededdir");
+//    }
+//    else
+//    {
+//        Console.WriteLine("nezere alinmayib");
+//    }
+
+
+
+#endregion
+
+#region task1 new
+Console.WriteLine("Reqemleri daxil edin");
+int num1=int.Parse(Console.ReadLine());
 int num2 = int.Parse(Console.ReadLine());
 int sum=num1+ num2;
-
-    if (sum <= 99 && sum >=10)
+int m= 10;
+int count = 1;
+while(sum>=m)
+{
+    if (sum / m != 0)
     {
-        Console.WriteLine("Cem 2 reqemli ededdir");
+        sum = sum / m;
+        count += 1;
     }
-
-    else if (sum>=100 && sum <= 999)
-    {
-        Console.WriteLine("Cem 3 reqemli ededdir");
-    }
-    else
-    {
-        Console.WriteLine("nezere alinmayib");
-    }
-
-
+}
+Console.Write("daxil etdiyiniz reqemlerin cemi:");
+Console.Write(count);
+Console.WriteLine(" mertebelidir");
 
 #endregion
