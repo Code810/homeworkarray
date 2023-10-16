e4b9eb4975d676677c89d0da5a0c12e6592a8d3e
code810 <nadirssh@code.edu.az>
2023-10-16 15:35:18 +0400

    caculator ready

--- arrayhomework/Program.cs
+++ arrayhomework/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+
 #region homework task1
 
 //Console.WriteLine("reqemleri daxil edin:");
@@ -45,31 +46,83 @@
 
 #endregion
 
-#region cupboard and door
+#region task2 cupboard and door
 
-Console.WriteLine("Skafin olculerini daxil edin");
-Console.Write("eni:");
-int eni = int.Parse(Console.ReadLine());
-Console.Write("hundurluyu:");
-int hundurluk = int.Parse(Console.ReadLine());
-Console.Write("uzunluq:");
-int uzunluq = int.Parse(Console.ReadLine());
+//Console.WriteLine("Skafin olculerini daxil edin");
+//Console.Write("eni:");
+//int eni = int.Parse(Console.ReadLine());
+//Console.Write("hundurluyu:");
+//int hundurluk = int.Parse(Console.ReadLine());
+//Console.Write("uzunluq:");
+//int uzunluq = int.Parse(Console.ReadLine());
 
-int e = 100;
-int h = 200;
+//int e = 100;
+//int h = 200;
 
-if ((eni <= e && hundurluk <= h) || (eni <= e && uzunluq <= h) || (uzunluq <= e && hundurluk <= h) || (uzunluq <= e && eni <= h)
-    || (hundurluk <= e && uzunluq <= h) || (hundurluk <= e && eni <= h)
-    )
+//if ((eni <= e && hundurluk <= h) || (eni <= e && uzunluq <= h) || (uzunluq <= e && hundurluk <= h) || (uzunluq <= e && eni <= h)
+//    || (hundurluk <= e && uzunluq <= h) || (hundurluk <= e && eni <= h)
+//    )
+//{
+//    Console.WriteLine("skaf qapidan kecir");
+//}
+
+//else
+//{
+//    Console.WriteLine("skaf qapidan kecmir");
+
+//}
+
+
+#endregion
+
+#region mytask
+
+using System;
+while (true)
 {
-    Console.WriteLine("skaf qapidan kecir");
-}
 
+
+Console.WriteLine("Ededi daxi et");
+
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Prosesi daxil et"," * ", " - ", " / ", " + " );
+string prosess= Console.ReadLine(); 
+Console.WriteLine("Ededi daxi et");
+int num2 = int.Parse(Console.ReadLine());
+string[] proses = { "*", "-", "/", "+" };
+int result;
+if (prosess == proses[0])
+{
+    result = num1 * num2;
+    Console.Write("Cavab:");
+    Console.WriteLine(result);
+        Console.WriteLine();
+    }
+else if (prosess == proses[1])
+{
+    result = num1 - num2;
+    Console.Write("Cavab:");
+    Console.WriteLine(result);
+        Console.WriteLine();
+    }
+else if (prosess == proses[2])
+{
+    result = num1 / num2;
+    Console.Write("Cavab:");
+    Console.WriteLine(result);
+        Console.WriteLine();
+    }
+else if (prosess == proses[3])
+{
+    result=num1 + num2;
+    Console.Write("Cavab:");
+    Console.WriteLine(result);
+        Console.WriteLine();
+    }
 else
 {
-    Console.WriteLine("skaf qapidan kecmir");
-
+    Console.WriteLine("Duzgun daxil etmediniz");
+        Console.WriteLine();
 }
-
-
+}
 #endregion
